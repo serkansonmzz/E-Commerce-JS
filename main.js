7f28f09fb06d3cc7f31f1b4384824f6ffdffa4b2
serkansonmzz <serkansonmzz@gmail.com>
2023-10-16 14:27:38 +0300

    fetchProducts

--- main.js
+++ main.js
@@ -49,3 +49,46 @@ function renderCategories(categories) {
     categoryList.appendChild(categoryDiv);
   });
 }
+
+// data değişkenini global scope'dea tanımladık
+// bu sayde bütün fonksiyonlar bu değere erişebilecek
+let data;
+
+// ürünler verisini çeken fonksiyon
+async function fetchProducts() {
+  try {
+    // api'a istek at
+    const response = await fetch(`${baseUrl}/products`);
+    // gelen cevabı işle
+    data = await response.json();
+    // ekrana bas
+    renderProducts(data);
+  } catch (err) {
+    // alert('Ürünleri alırken bir hata oluştu');
+  }
+}
+
+// ürünleri ekran basre
+function renderProducts(products) {
+  // her bir ürün için bir ürün kartı oluşturma
+  const cardsHTML = products
+    .map(
+      (product) => `
+         <div class="card">
+            <div class="img-wrapper">
+                <img src="${product.image}" />
+            </div>
+            <h4>${product.title}</h4>
+            <h4>${product.category}</h4>
+            <div class="info">
+              <span>${product.price}$</span>
+              <button onclick="addToBasket(${product.id})">Sepete Ekle</button>
+            </div>
+          </div>
+  `
+    )
+    .join(" ");
+
+  // hazırladığımız html'i ekrana basma
+  productList.innerHTML = cardsHTML;
+}
